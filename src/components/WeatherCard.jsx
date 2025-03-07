5d761e5619af9a1a7bd7823b5e64a9712583c377
arturaarma <artur.aarma@gmail.com>
2025-03-07 15:12:58 +0200

    Fixed some dark mode colors

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -11,25 +11,25 @@ const WeatherCard = ({weather, weatherData}) => {
         <div className="bg-blue-800 dark:bg-gray-800 text-white p-6 rounded-xl shadow-lg w-full mb-6">
             <h2 className="text-xl font-semibold">{weatherData[0] ? weatherData[0].city : "no info"}</h2>
             <p className="text-3xl">{weatherData[0] ? weatherData[0].temperature : "No info"}°C</p>
-            <p className="text-sm">Humidity:{weatherData[0] ? weatherData[0].humidity : "No info"}% | Wind: {weatherData[0] ? weatherData[0].wind : "No info"} km/h</p>
+            <p className="text-sm">Humidity: {weatherData[0] ? weatherData[0].humidity : "No info"}% | Wind: {weatherData[0] ? weatherData[0].wind : "No info"} km/h</p>
         </div>
 
         <div className="bg-blue-800 dark:bg-gray-800 text-white p-6 rounded-xl shadow-lg w-full mb-6">
             <h2 className="text-xl font-semibold">{weatherData[1] ? weatherData[1].city : "no info"}</h2>
             <p className="text-3xl">{weatherData[1] ? weatherData[1].temperature : "No info"}°C</p>
-            <p className="text-sm">Humidity:{weatherData[1] ? weatherData[1].humidity : "No info"}% | Wind: {weatherData[1] ? weatherData[1].wind : "No info"} km/h</p>
+            <p className="text-sm">Humidity: {weatherData[1] ? weatherData[1].humidity : "No info"}% | Wind: {weatherData[1] ? weatherData[1].wind : "No info"} km/h</p>
         </div>
 
         <div className="bg-blue-800 dark:bg-gray-800 text-white p-6 rounded-xl shadow-lg w-full mb-6">
             <h2 className="text-xl font-semibold">{weatherData[2] ? weatherData[2].city : "no info"}</h2>
             <p className="text-3xl">{weatherData[2] ? weatherData[2].temperature : "No info"}°C</p>
-            <p className="text-sm">Humidity:{weatherData[2] ? weatherData[2].humidity : "No info"}% | Wind: {weatherData[2] ? weatherData[2].wind : "No info"} km/h</p>
+            <p className="text-sm">Humidity: {weatherData[2] ? weatherData[2].humidity : "No info"}% | Wind: {weatherData[2] ? weatherData[2].wind : "No info"} km/h</p>
         </div>
 
         <div className="bg-blue-800 dark:bg-gray-800 text-white p-6 rounded-xl shadow-lg w-full mb-6">
             <h2 className="text-xl font-semibold">{weatherData[3] ? weatherData[3].city : "no info"}</h2>
             <p className="text-3xl">{weatherData[3] ? weatherData[3].temperature : "No info"}°C</p>
-            <p className="text-sm">Humidity:{weatherData[3] ? weatherData[3].humidity : "No info"}% | Wind: {weatherData[3] ? weatherData[3].wind : "No info"} km/h</p>
+            <p className="text-sm">Humidity: {weatherData[3] ? weatherData[3].humidity : "No info"}% | Wind: {weatherData[3] ? weatherData[3].wind : "No info"} km/h</p>
         </div>
 
         
